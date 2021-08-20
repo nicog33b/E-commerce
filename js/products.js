5ab16050407db59857341abe6a9d9e6c4ebe5c6a
reptilcuantico99 <88388887+reptilcuantico99@users.noreply.github.com>
2021-08-20 02:22:58 -0300

    Entrega 1(casi terminada).
    Hasta aquí esta logrado casi todo el contenido de la entrega1 faltan detalles mas que nada visuales.

--- js/products.js
+++ js/products.js
@@ -1,6 +1,50 @@
+//Analizando veo que puedo reutilizar el codigo usado en clase.
+
+function mostrarProductosLista(array){
+//
+//creo una variable que contendra en un string el contenido a agregar al html
+    let htmlContentToAppend = "";
+    //el for recorre el array cada atributo antes de pasar al proximo indice del array obteniendo los datos necesarios
+    //para rellenar los datos que nos interesan.-
+    for(let i = 0; i < array.length; i++){
+        let productos = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + productos.imgSrc + `" alt="` + "Imagen-de-producto"+ `" class="imagenProducto">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="nombreProducto">`+ productos.name+`</h4>
+                     
+                    </div>
+
+                </div>
+                <small class="descripcionProducto">` + productos.description+ `</small>
+            </div>
+        </div>
+        `
+
+        document.getElementById("lista-container").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener("DOMContentLoaded",function(e){
+
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productosArray= resultObj.data;
+            hideSpinner();
+            //Muestro las categorías ordenadas
+            //console.log(mostrarProductosLista(productosArray));
+            mostrarProductosLista(productosArray)
+        }
+    });
 
 });
