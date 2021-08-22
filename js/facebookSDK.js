d408742c4844b2b3c2f71a1d6bbb08791e8cc3f1
reptilcuantico99 <88388887+reptilcuantico99@users.noreply.github.com>
2021-08-22 17:03:45 -0300

    Update facebookSDK.js

--- js/facebookSDK.js
+++ js/facebookSDK.js
@@ -3,10 +3,16 @@ function onLogin(){
 if(Response.authResponse){
   FB.api('/me?fields=email,name,last_name',(Response)=>{
 console.log(Response);
+
   })
 }
   })
 }
+
+FB.getLoginStatus(function(response) {
+  statusChangeCallback(response);
+  console.log(response)
+});
 
   window.fbAsyncInit = function() {
     FB.init({
