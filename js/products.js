e2dabe6d5fca906628f16e2c0a7a4092b5fedb22
reptilcuantico99 <88388887+reptilcuantico99@users.noreply.github.com>
2021-08-24 18:18:55 -0300

    asd

--- js/products.js
+++ js/products.js
@@ -1,4 +1,5 @@
 //Analizando veo que puedo reutilizar el codigo usado en clase.
+let arrProductosActual = [];
 
 function mostrarProductosLista(array){
 //
@@ -7,29 +8,33 @@ function mostrarProductosLista(array){
     //el for recorre el array cada atributo antes de pasar al proximo indice del array obteniendo los datos necesarios
     //para rellenar los datos que nos interesan.-
     for(let i = 0; i < array.length; i++){
-        let productos = array[i];
+        let product = array[i];
 
         agregarHtml += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + productos.imgSrc + `" alt="` + "Imagen-de-producto"+ `" class="imagenProducto">
+        <a href="product-info.html" class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col-3">
+                <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">`+ product.name +`</h4>
+                    <small class="text-muted">` + product.soldCount + ` venididos</small>
                 </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="nombreProducto">`+ productos.name+`</h4>
-                     
-                    </div>
-
+                <div class="d-flex w-100 justify-content-between" >
+                <p class="mb-1">` + product.description + `</p>
+                <p> ` + product.currency + ``+ product.cost +`
+                </p>
                 </div>
-                <small class="descripcionProducto">` + productos.description+ `</small>
             </div>
         </div>
-        `
+    </a>
+    `
 
         document.getElementById("lista-container").innerHTML = agregarHtml;
     }
 }
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -44,7 +49,18 @@ document.addEventListener("DOMContentLoaded",function(e){
             //Muestro las categorías ordenadas
             //console.log(mostrarProductosLista(productosArray));
             mostrarProductosLista(objProductos)
+         console.log(objProductos[0].cost);
         }
+    });
+    
+    document.getElementById("btnLimpiar").addEventListener("click", function(){
+        document.getElementById("rangeFilterCountMin").value = "";
+        document.getElementById("rangeFilterCountMax").value = "";
+
+        minCount = undefined;
+        maxCount = undefined;
+
+        showProductsList();
     });
 
 });
