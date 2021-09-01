1121b689a52b8c62fd95cabfb845260dec2f1218
reptilcuantico99 <88388887+reptilcuantico99@users.noreply.github.com>
2021-09-01 14:14:35 -0300

    Avances 1/9 temprano.
    Logre que el usuario logeado y el no logeado tengan un menu distinto, basado en lo necesario para cada uno.

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,25 @@
+//obtiene usuario logeado
 const userOn = document.getElementById("usuario");
+//obtiene el boton que cierra la sesion.
 const close = document.getElementById("cerrar")
+//la variable username contiene un array con los datos de iniio de sesion del usuario.
 let username= JSON.parse(localStorage.getItem('userConectado'))
+//obtiene 
+let userNombre=document.getElementById("userNombre");
+
+
 if(username != null){
  userOn.innerText=username[0].usuario;
-}else{
+ }else{
+     //Cambia el texto del boton
     userOn.innerText="Inicia Sesion"
+    //no muestra las opciones cerrar sesion y carrito porque no esta logeado.
+    document.getElementById("mostrarOcultar").style.display="none";
+    //redirije al index para poder logear o registrarse en el hipotetico caso que existiera registro.
+    document.getElementById("usuario").addEventListener("click",function(){
+        window.location.href="index.html";
+      })
+    
 }
 console.log(username[0].usuario);
 
