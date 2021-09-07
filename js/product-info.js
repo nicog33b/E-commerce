5263cfe0ef3b6dc8207b22d965f0b2bb86009363
reptilcuantico99 <88388887+reptilcuantico99@users.noreply.github.com>
2021-09-07 13:26:55 -0300

    Product.info comienzo
    agregando caja de comentarios y puntuación para los usuarios.

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+           productoInfo = resultObj.data;
+          
+        }
+    });
 });
