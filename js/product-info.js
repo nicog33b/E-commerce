8f53012df34c6d9cc3cf97fe804c41ac2ba428a7
reptilcuantico99 <88388887+reptilcuantico99@users.noreply.github.com>
2021-09-09 04:47:38 -0300

    8/9
    asd

--- js/product-info.js
+++ js/product-info.js
@@ -1,10 +1,12 @@
-let productoInfo=[];
+
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
            productoInfo = resultObj.data;
+           document.getElementById("nombreProducto").innerText=productoInfo.name;
           
         }
     });
-    document.getElementById("nombreProducto").innerText=productoInfo.name;
+   
+     
 });
