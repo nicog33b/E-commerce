97695d442e859c67a44ec89700a7cdaa9648cd10
reptilcuantico99 <88388887+reptilcuantico99@users.noreply.github.com>
2021-09-15 13:24:24 -0300

    15/9
    sssss

--- js/product-info.js
+++ js/product-info.js
@@ -21,9 +21,108 @@ llenarTablaDatosProducto=()=>{
     document.getElementById("monedaProducto").innerHTML=productoInfo.currency;
     document.getElementById("disponibleProducto").innerHTML=productoInfo.soldCount;
     document.getElementById("categoriaProducto").innerHTML=productoInfo.category;
-  
+
 }
+let starTipo=0;
+star1=()=>{
+    document.getElementById("star1").className="fa fa-star checked"
+    document.getElementById("star2").className="fa fa-star"
+    document.getElementById("star3").className="fa fa-star"
+    document.getElementById("star4").className="fa fa-star"
+    document.getElementById("star5").className="fa fa-star"
+}
+
+star2=()=>{
+    document.getElementById("star1").className="fa fa-star checked"
+    document.getElementById("star2").className="fa fa-star checked" 
+    document.getElementById("star3").className="fa fa-star"
+    document.getElementById("star4").className="fa fa-star"
+    document.getElementById("star5").className="fa fa-star"
+}
+
+
+star3=()=>{
+    document.getElementById("star1").className="fa fa-star checked"
+    document.getElementById("star2").className="fa fa-star checked"
+    document.getElementById("star3").className="fa fa-star checked"
+    document.getElementById("star4").className="fa fa-star"
+    document.getElementById("star5").className="fa fa-star"
+}
+
+
+star4=()=>{
+    document.getElementById("star1").className="fa fa-star checked"
+    document.getElementById("star2").className="fa fa-star checked"
+    document.getElementById("star3").className="fa fa-star checked"
+    document.getElementById("star4").className="fa fa-star checked"
+    document.getElementById("star5").className="fa fa-star"
+}
+
+
+star5=()=>{
+    document.getElementById("star1").className="fa fa-star checked"
+    document.getElementById("star2").className="fa fa-star checked"
+    document.getElementById("star3").className="fa fa-star checked"
+    document.getElementById("star4").className="fa fa-star checked"
+    document.getElementById("star5").className="fa fa-star checked"
+}
+
+function mostrarListaComentarios(comment){
+    //
+    //creo una variable que contendra en un string el contenido a agregar al html
+        let agregarComentario= "";
+        //el for recorre el array cada atributo antes de pasar al proximo indice del array obteniendo los datos necesarios
+        //para rellenar los datos que nos interesan.-
+        for(let i = 0; i < comment.length; i++){
+            let comentario= comment[i];
+            agregarComentario += `
+           
+              <div class="container">
+                <div class="row align-items-center justify-content-center">
+                      <div class="col-12 col-md-7">
+                        <p class="text-h3 mt-2"><strong> `+ comentario.user +`</strong>   
+                          &nbsp;
+                          <span  class="fa fa-star checked"></span>
+                          <span  class="fa fa-star checked"></span>
+                          <span  class="fa fa-star checked"></span>
+                          <span  class="fa fa-star"></span>
+                          <span  class="fa fa-star"></span></p>
+                    <p class="comentarioUsuario" >
+                    `+ comentario.description +`
+                    </p>
+                 
+                  </div>
+                  `
+                
+            document.getElementById("cajaComentarios").innerHTML = agregarComentario;
+        
+        }
+    }
+
+
 document.addEventListener("DOMContentLoaded", function(e){
+  
+    document.getElementById("star1").addEventListener("click", function(){
+        star1();
+});
+
+document.getElementById("star2").addEventListener("click", function(){
+    star2();
+    });
+
+    document.getElementById("star3").addEventListener("click", function(){
+        star3();
+        });
+
+        document.getElementById("star4").addEventListener("click", function(){
+            star4();
+            });
+
+            document.getElementById("star5").addEventListener("click", function(){
+                star5();
+                });
+
+
     getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
            productoInfo = resultObj.data;
@@ -35,9 +134,19 @@ document.addEventListener("DOMContentLoaded", function(e){
            llenarDescripcion();
            //mostrar aviso de stock y consultar al vendedor
            document.getElementById("avisoStock").innerHTML="Hay "+productoInfo.soldCount+" Disponibles"; 
-         //llenar los datos de la tabla de información del producto ref linea 19
+           //llenar los datos de la tabla de información del producto ref linea 19
           llenarTablaDatosProducto()
           
+        }
+
+    
+    });
+
+    
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+           productoComentario = resultObj.data;
+           mostrarListaComentarios(productoComentario);
         }
 
     
