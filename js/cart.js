e369ed857c22ef88109e26cb2ed82f6712918b49
reptilcuantico99 <88388887+reptilcuantico99@users.noreply.github.com>
2021-10-08 19:50:48 -0300

    ggh

--- js/cart.js
+++ js/cart.js
@@ -2,5 +2,7 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    https://japdevdep.github.io/ecommerce-api/cart/654.json
 
+    
 });
