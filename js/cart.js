82560057706c78064b3e01e77fbac93f777e8807
Nicolás García <88388887+reptilcuantico99@users.noreply.github.com>
2021-10-10 02:35:38 -0300

    knnk

--- js/cart.js
+++ js/cart.js
@@ -1,22 +1,51 @@
-let arrayCarrito=[];
+let arrayCarrito = [];
 
-agregarProductosRelacionados=()=>{
+agregarItemsAlCarrito = () => {
+
+    let agregarAlCarrito = "";
+    for (let i = 0; i < arrayCarrito.length; i++) {
+        let carrito = arrayCarrito[i];
+        agregarAlCarrito += `
+       
+        
+        <td data-th="Product">
+          <div class="row">
+            <h2 class="nomargin">&nbsp;&nbsp;&nbsp;Product 2</h4>
+        
+            <div class="col-sm-10">
+              <div class="col-sm-2 hidden-xs"><img src="img/cat1.jpg" alt="..." class="img-responsive" width="200" ></div>
+            
+            </div>
+          </div>
+        </td>
+        <td data-th="Price">$1.99</td>
+        <td data-th="Quantity">
+          <input type="number" class="form-control text-center" value="1">
+        </td>
+        <td data-th="Subtotal" class="text-center">1.99</td>
+      
+
+                  `
+
+        document.getElementById("itemsDelCarrito").innerHTML = agregarAlCarrito;
+
+    }
 
 
 
-    
 }
 
 
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
 
     getJSONData(CART_INFO2_URL).then(function (carrito) {
         if (carrito.status === "ok") {
-            buyCarInfo= carrito.data;
-            arrayCarrito=carrito.data;    
+            buyCarInfo = carrito.data;
+            arrayCarrito = carrito.data.articles;
+            console.log(arrayCarrito)
+            agregarItemsAlCarrito()
 
-alert(arrayCarrito)
 
 
 
