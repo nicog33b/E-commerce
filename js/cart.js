63abde3d44aafa1c2e6067698d6fdd7b8450e02c
reptilcuantico99 <88388887+reptilcuantico99@users.noreply.github.com>
2021-10-15 10:32:24 -0300

    pre entrega de proyecto
    en proceso el carrito

--- js/cart.js
+++ js/cart.js
@@ -1,18 +1,49 @@
 let arrayCarrito = [];
 let tipoMoneda = 0;
 
-   cargarSubtotalItem = () =>{
-    for (var i = 0; i < arrayCarrito.length; i++) {
-var productActual = parseFloat(document.getElementById("producto"[i]).value)
-console.log(productActual)
+loadPaymentsInfo = () => {
+  let subtotal=0;
+  for (var i = 0; i < arrayCarrito.length; i++) {
+    let carrito = arrayCarrito[i];
+    subtotal=subtotal+parseFloat(document.getElementById("subtotal" + [i]).value);
   
+      
+
+  }
+  document.getElementById("subtotal").innerHTML =subtotal; 
+
+}
+
+
+loadItemsSubtotal = () => {
+  for (var i = 0; i < arrayCarrito.length; i++) {
+    let carrito = arrayCarrito[i];
+    var productActual = parseFloat(document.getElementById("producto" + [i]).value)
+
+    let unitPrice = 0;
+    if (carrito.currency === "USD") {
+      unitPrice = carrito.unitCost
+    } else if (carrito.currency = "UYU") {
+      unitPrice = (carrito.unitCost / 40).toFixed(2);
     }
+    document.getElementById("subtotal" + [i]).innerHTML = unitPrice * productActual;
+    
 
-   }
+  }
+  let subtotal=0;
+  for (var i = 0; i < arrayCarrito.length; i++) {
 
-   loadCountrySelect = () => {
+    newSubtotal=parseFloat(document.getElementById("subtotal" + [i]).textContent);
+    subtotal=subtotal+newSubtotal;
+    document.getElementById("subtotal").innerHTML="$"+subtotal;
+
+  }
+
+}
+
+loadCountrySelect = () => {
   var departamentos = ["MONTEVIDEO", "ARTIGAS", "CANELONES", "CERRO LARGO", "COLONIA", "DURAZNO", " FLORES", "FLORIDA",
-   " LAVALLEJA", "MALDONADO", "PAYSANDU", "RIO NEGRO"]; //array de los departamentos.
+    " LAVALLEJA", "MALDONADO", "PAYSANDU", "RIO NEGRO"]; //array de los departamentos.
   var select = document.getElementById("selectCountry"); //Seleccionamos el select
 
   for (var i = 0; i < departamentos.length; i++) {
@@ -36,7 +67,7 @@ addItemToCart = () => {
   let agregarAlCarrito = "";
   for (let i = 0; i < arrayCarrito.length; i++) {
     let carrito = arrayCarrito[i];
-  
+
 
     //si esta en pesos uruguayos los transforma en dolares.
     let unitPrice = 0;
@@ -62,9 +93,9 @@ addItemToCart = () => {
         <div class="arregloTabla">
         <td class="text-cente"data-th="Price">`+ unitPrice + `<br>` + "USD" + `</td>
         <td data-th="Quantity">
-          <input id="`+ "producto" + numProd + `" oninput="cargarSubtotalItem()" type="number" class="form-control text-center" value="1">
+          <input id="`+ "producto" + numProd + `" min="1" pattern="^[0-9]+" oninput="loadItemsSubtotal()" type="number" class="form-control text-center" value="1">
         </td>
-        <td id="`+ "subtotal" + numProd + `" data-th="Subtotal" class="text-center">` + +`</td>
+        <td id="`+ "subtotal" + numProd + `"  data-th="Subtotal" class="text-center">` + +`</td>
         </tr>
         </div>
         `
@@ -92,8 +123,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
       arrayCarrito = carrito.data.articles;
       console.log(arrayCarrito)
       addItemToCart()
-
-
+      loadItemsSubtotal()
+ 
 
 
 
